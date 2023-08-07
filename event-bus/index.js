9ab5665836dda4be039539040e3ebe77b73f27d8
nicovui <github.com>
2023-08-07 15:52:19 +0200

    end of simple app = kubernetes depl config

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,22 +7,20 @@ app.use(bodyParser.json());
 
 const events = [];
 
+const post = (host, port, path, event) => {
+  axios.post(`http://${host}:${port}${path}`, event).catch(err => {
+    console.log(err.message + ` : http://${host}:${port}${path}`);
+  });
+}
+
 app.post('/events', (req, res) => {
   const event = req.body;
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch(err => {
-    console.log(err.message + ' : http://localhost:4000/events');
-  });
-  axios.post('http://localhost:4001/events', event).catch(err => {
-    console.log(err.message + ' : http://localhost:4001/events');
-  });
-  axios.post('http://localhost:4002/events', event).catch(err => {
-    console.log(err.message + ' : http://localhost:4002/events');
-  });
-  axios.post('http://localhost:4003/events', event).catch(err => {
-    console.log(err.message + ' : http://localhost:4003/events');
-  });
+  post('posts-clusterip-svc', '4000', '/events', event);
+  post('comments-svc', '4001', '/events', event);
+  post('query-svc', '4002', '/events', event);
+  post('moderation-svc', '4003', '/events', event);
 
   res.send({status: 'OK'});
 });
